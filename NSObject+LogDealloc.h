c06c337bc97d97dfdfd97269722e9fecdb290237
Juniorchen <Juniorchen2010@163.com>
2014-03-18 10:24:58 +0800

    LogMSG when dealloced

---
+++ NSObject+LogDealloc.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+LogDealloc.h
+//  JASidePanels
+//
+//  Created by Juniorchen on 14-3-18.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (LogDealloc)
+- (void)logOnDealloc;
+@end
